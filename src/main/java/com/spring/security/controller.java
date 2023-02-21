d987e08be3935da9e2a0bbb30cd8bfe8465bd44f
Ranveer Singh <ranveer.singh@ranveersss210-ub.successivetech.com>
2023-02-21 12:18:32 +0530

    feat: add basic controller and spring security dependency only

---
+++ src/main/java/com/spring/security/controller.java
@@ -0,0 +1,15 @@
+package com.spring.security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class controller {
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        return "Hi welcome! Ranveer";
+    }
+}
